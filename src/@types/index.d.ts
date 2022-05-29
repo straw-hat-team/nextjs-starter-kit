79e84a78a25adceb73335dd5b885029d76253f3b
Yordis Prieto Lazo <yordis@hmbradley.com>
2022-05-28 22:23:28 -0400

    chore: fix type issue

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -7,3 +7,10 @@ declare module '*.module.scss' {
   const classes: { readonly [key: string]: string };
   export default classes;
 }
+
+// TODO: remove once https://github.com/testing-library/jest-dom/issues/426 is resolved
+import type { TestingLibraryMatchers } from '@types/testing-library__jest-dom/matchers';
+
+declare module '@jest/expect' {
+  export interface Matchers<R = void, T = {}> extends TestingLibraryMatchers<typeof expect.stringContaining, R> {}
+}
